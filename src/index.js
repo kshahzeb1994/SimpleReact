360b3eb303b9257d4404c569b6f5daf2058dc484
kshahzeb1994 <kshahzeb1994@gmail.com>
2018-10-03 13:20:16 -0700

    added youtube api response length

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyCPShpq9wPLbO6K-RHozsI9vBa_jgTZhs8';
 
 //create a new component that produces some HTML
-const App = () => {
+class App extends Component {
+  constructor(props) {
+    super(props);
+  	// console.log(props);
+    this.state = { videos : [] };
+
+    YTSearch({key: API_KEY, term:'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
     return (
       <div>
-            <SearchBar />
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
+  }
 }
 
 //Take this component's generated HTML and put is on the page (in the DOM)
