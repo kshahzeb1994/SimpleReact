8f50e7c63eee31b8a365c2b1ee7c4026984dab32
kshahzeb1994 <kshahzeb1994@gmail.com>
2018-10-02 22:32:25 -0700

    Added Api key + Youtube search package

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-//create a new component that produces some HTML
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyCPShpq9wPLbO6K-RHozsI9vBa_jgTZhs8';
+
+//create a new component that produces some HTML
+const App = () => {
+    return <div>viva la rasa! I Lie, I Cheat, I Steal</div>;
+}
 
 //Take this component's generated HTML and put is on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
